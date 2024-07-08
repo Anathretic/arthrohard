63c8c1f2f63f98356caadc866ba8cbcdae715e33
Konrad <konrad.wojtylo.9@gmail.com>
2024-07-08 17:18:48 +0200

    Created composition section and made minor improvements

--- src/components/sections/Navbar.jsx
+++ src/components/sections/Navbar.jsx
@@ -28,7 +28,7 @@ export const Navbar = () => {
 	return (
 		<header>
 			<div className='fixed top-0 w-full md:h-[120px] h-[80px] flex md:justify-center justify-between items-center py-4 mf:py-2 z-10 transition duration-300 mr-auto tracking-widest bg-white shadow-gray-400 shadow-md'>
-				<div className='md:flex-[0.8] xl:ml-48 2xl:ml-64 2xl:flex-[0.5] flex-initial justify-center items-center'>
+				<div className='md:flex-[0.8] xxxl:ml-20 2xl:flex-[0.5] flex-initial justify-center items-center'>
 					<div className='flex items-center mf:mx-2'>
 						<h2 className='cursor-pointer p-2 mf:p-0 uppercase font-semibold md:text-3xl text-xl' onClick={handleLogo}>
 							arthro<span className='text-red-600'>hard</span>
@@ -36,13 +36,13 @@ export const Navbar = () => {
 					</div>
 				</div>
 				<nav>
-					<ul className='text-black md:flex hidden list-none flex-row justify-between items-center flex-initial xxxl:ml-40'>
+					<ul className='text-black md:flex hidden list-none flex-row justify-between items-center flex-initial xxxl:ml-96'>
 						{navbarItems.map(({ title, section }) => (
 							<NavbarItem key={title} title={title} section={section} classProps={'hover:text-red-600'} />
 						))}
 					</ul>
 				</nav>
-				<div className='xl:flex hidden w-1/4' />
+				<div className='xxxl:flex hidden w-1/4' />
 				<div className='flex relative'>
 					{toggleMenu || (
 						<HiMenuAlt4
