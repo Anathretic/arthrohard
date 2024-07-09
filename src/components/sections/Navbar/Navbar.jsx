1fe13c2fd811d25682338de32c50cae748482450
Konrad <konrad.wojtylo.9@gmail.com>
2024-07-09 15:21:30 +0200

    Refactored code and made some improves as preparation for release

---
+++ src/components/sections/Navbar/Navbar.jsx
@@ -0,0 +1,78 @@
+import { useState } from 'react';
+import { AiOutlineClose } from 'react-icons/ai';
+import { HiMenuAlt4 } from 'react-icons/hi';
+import { NavbarItem } from './components/NavbarItem';
+
+const navbarItems = [
+	{
+		title: 'co nas wyróżnia',
+		section: 'co-nas-wyroznia',
+	},
+	{
+		title: 'skład',
+		section: 'sklad',
+	},
+	{
+		title: 'produkty',
+		section: 'produkty',
+	},
+];
+
+export const Navbar = () => {
+	const [toggleMenu, setToggleMenu] = useState(false);
+
+	const handleLogo = () => {
+		window.location.href = '#';
+	};
+
+	return (
+		<header>
+			<div className='fixed top-0 w-full md:h-[120px] h-[80px] flex md:justify-center justify-between items-center py-4 mf:py-2 z-10 transition duration-300 mr-auto tracking-widest bg-white shadow-gray-400 shadow-md'>
+				<div className='md:flex-[0.8] xxxl:ml-20 2xl:flex-[0.5] flex-initial justify-center items-center'>
+					<div className='flex items-center mf:mx-2'>
+						<h2 className='cursor-pointer p-2 mf:p-0 uppercase font-semibold md:text-3xl text-xl' onClick={handleLogo}>
+							arthro<span className='text-red-600'>hard</span>
+						</h2>
+					</div>
+				</div>
+				<nav>
+					<ul className='text-black md:flex hidden list-none flex-row justify-between items-center flex-initial xxxl:ml-96'>
+						{navbarItems.map(({ title, section }) => (
+							<NavbarItem key={title} title={title} section={section} classProps={'hover:text-red-600'} />
+						))}
+					</ul>
+				</nav>
+				<div className='xxxl:flex hidden w-1/4' />
+				<div className='flex relative'>
+					{toggleMenu || (
+						<HiMenuAlt4
+							fontSize={32}
+							className='text-black md:hidden cursor-pointer mx-2 sm:mx-4'
+							onClick={() => setToggleMenu(true)}
+						/>
+					)}
+					{toggleMenu && (
+						<nav>
+							<ul className='z-10 fixed top-0 -right-2 p-3 w-[70vw] h-screen shadow-2xl md:hidden list-none flex flex-col justify-start items-center rounded-md text-white bg-black animate-slide-in'>
+								<li className='text-xl self-start my-2 cursor-pointer'>
+									<AiOutlineClose fontSize={28} onClick={() => setToggleMenu(false)} />
+								</li>
+								{navbarItems.map(({ title, section }) => (
+									<NavbarItem
+										key={title}
+										title={title}
+										section={section}
+										classProps={'mb-5'}
+										onClick={() => {
+											setToggleMenu(false);
+										}}
+									/>
+								))}
+							</ul>
+						</nav>
+					)}
+				</div>
+			</div>
+		</header>
+	);
+};
---
+++ src/components/sections/WhatSetsUsApartSection/WhatSetsUsApart.jsx
@@ -0,0 +1,101 @@
+import { Element } from 'react-scroll';
+
+import FirstContainerImage from '../../../images/what-sets-us-apart-section/first-container-image.png';
+import SecondContainerImage1 from '../../../images/what-sets-us-apart-section/second-container-image-1.png';
+import SecondContainerImage2 from '../../../images/what-sets-us-apart-section/second-container-image-2.png';
+import ThirdContainerImage from '../../../images/what-sets-us-apart-section/third-container-image.png';
+import VectorImage from '../../../images/what-sets-us-apart-section/vector-image.svg';
+
+export const WhatSetsUsApart = () => {
+	return (
+		<Element name='co-nas-wyroznia'>
+			<section className='w-full pb-10'>
+				<div className='w-full min-h-screen flex justify-center items-center flex-col'>
+					<h3 className='h-full pb-2 max-2xl:pt-12 pt-20 lg:pb-4 text-center 2xl:text-4xl md:text-3xl nsm:text-2xl text-2xl font-semibold section-title-gradient'>
+						Co wyróżnia nasz preparat
+					</h3>
+					<div className='h-[1px] border border-special-black w-[50px] lg:w-[80px] 2xl:w-[120px]' />
+					<div className='w-full flex justify-center flex-col 2xl:flex-row lg:justify-between items-center pt-16 2xl:pt-48 xxxl:px-48 text-center'>
+						<div className='flex justify-center items-center flex-col max-w-[400px] max-lg:px-4'>
+							<h4 className='font-semibold text-xl lg:text-2xl'>
+								Innowacyjny dodatek - <br /> Czarciego Pazura
+							</h4>
+							<div className='h-[1px] border border-special-black w-[50px] lg:w-[80px] 2xl:w-[100px] my-5' />
+							<p className='pb-5'>
+								Zapewnia dodatkowe wsparcie w zwalczaniu stanów bólowych i zapalnych. Skoncentrowane składniki aktywne,
+								opracowane we współpracy z lekarzami weterynarii, przynoszą szybkie i zauważalne efekty, szczególnie
+								korzystne dla zwierząt z poważnymi problemami stawowymi.
+							</p>
+							<div className='relative w-full h-full flex items-center justify-center'>
+								<img
+									src={FirstContainerImage}
+									alt='Obraz przedstawiający Czarciego Pazura'
+									className='hover:border-b lg:hover:border-special-black border-b border-transparent pb-4 transition duration-300'
+								/>
+								<img src={VectorImage} alt='Grafika upiększająca obrazek' className='first-container-vector-image-1' />
+								<img
+									src={VectorImage}
+									alt='Grafika upiększająca obrazek'
+									className='first-container-vector-image-2 hidden nsm:block'
+								/>
+							</div>
+						</div>
+						<div className='flex justify-center items-center flex-col max-w-[400px] max-lg:px-4'>
+							<h4 className='font-semibold text-xl lg:text-2xl max-lg:pt-8'>
+								Wygoda - <br />
+								dwa sposoby podawania
+							</h4>
+							<div className='h-[1px] border border-special-black w-[50px] lg:w-[80px] 2xl:w-[100px] my-5' />
+							<div className='pt-3 pb-5'>
+								<p className='pb-10 text-left '>
+									<span className='mr-3  bg-white rounded-full special-first-number-box-shadow font-semibold'>1</span>
+									Bezpośrednio do pyszczka
+								</p>
+								<p className='pb-10 text-left'>
+									<span className='mr-3 bg-white rounded-full special-second-number-box-shadow font-semibold'>2</span>
+									Zmieszany z karmą
+								</p>
+							</div>
+							<div className='relative w-full h-full flex items-center justify-center flex-col nsm:flex-row hover:border-b lg:hover:border-special-black border-b border-transparent pb-4 transition duration-300'>
+								<img
+									src={SecondContainerImage1}
+									alt='Obraz przedstawiający głaskanego pieska'
+									className='nsm:pr-2 nsm:pb-0 pb-4'
+								/>
+								<img
+									src={SecondContainerImage2}
+									alt='Obraz przedstawiający psa jedzącego z miski'
+									className='nsm:pl-2 nsm:pb-0 pb-4'
+								/>
+								<img src={VectorImage} alt='Grafika upiększająca obrazek' className='second-container-vector-image-1' />
+								<img src={VectorImage} alt='Grafika upiększająca obrazek' className='second-container-vector-image-2' />
+							</div>
+						</div>
+						<div className='flex justify-center items-center flex-col max-w-[400px] max-lg:px-4'>
+							<h4 className='font-semibold text-xl lg:text-2xl max-lg:pt-8'>
+								Skuteczność - <br />
+								skoncentrowana dawka
+							</h4>
+							<div className='h-[1px] border border-special-black w-[50px] lg:w-[80px] 2xl:w-[100px] my-5' />
+							<p className='pb-5'>
+								<span className='font-semibold'>Tylko jedna porcja dziennie.</span>
+								<br /> To nie tylko wygoda, ale również gwarancja, że Twoje zwierzę otrzymuje wszystko, czego
+								potrzebuje, aby cieszyć się zdrowiem i aktywnością na długo. Skorzystaj z zalet Arthrohard i zobacz
+								różnicę w komforcie życia swojego zwierzęcia.
+							</p>
+							<div className='relative w-full h-full flex items-center justify-center'>
+								<img
+									src={ThirdContainerImage}
+									alt='Obraz przedstawiający otwartą strzykawkę'
+									className='hover:border-b lg:hover:border-special-black border-b border-transparent pb-4 transition duration-300'
+								/>
+								<img src={VectorImage} alt='Grafika upiększająca obrazek' className='third-container-vector-image-1' />
+								<img src={VectorImage} alt='Grafika upiększająca obrazek' className='third-container-vector-image-2' />
+							</div>
+						</div>
+					</div>
+				</div>
+			</section>
+		</Element>
+	);
+};
